4c9fc80b484e4999a6ec16771e018da01e4ddf25
Rodrigo Sáez <Rodrigoandres@outlook.cl>
2025-11-19 18:52:17 -0300

    chore: inicializar backend Poké Mart con Spring Boot

---
+++ src/main/java/cl/pokemart/pokemart_backend/PokemartBackendApplication.java
@@ -0,0 +1,13 @@
+package cl.pokemart.pokemart_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemartBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemartBackendApplication.class, args);
+	}
+
+}
